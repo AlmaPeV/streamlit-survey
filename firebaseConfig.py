d8db4e8e6b4ffd49724ca79ab171476e3643fa1f
Alma Peña Valdes <147678686+AlmaPeV@users.noreply.github.com>
2024-10-25 19:44:37 -0600

    Update firebaseConfig.py

--- firebaseConfig.py
+++ firebaseConfig.py
@@ -3,12 +3,7 @@ from firebase_admin import credentials, firestore
 
 # Configurar Firebase
 def init_firebase():
-    cred = credentials.Certificate(import firebase_admin
-from firebase_admin import credentials
-
-cred = credentials.Certificate("C:\Users\Z20100\Downloads\maxdiff-a1891-firebase-adminsdk-enu7i-6a840fda73.json")
-firebase_admin.initialize_app(cred))  # Reemplaza esta ruta con la ruta de tu archivo JSON
+    cred = credentials.Certificate("C:\Users\Z20100\Downloads\maxdiff-a1891-firebase-adminsdk-enu7i-6a840fda73.json")  # Reemplaza esta ruta con la ruta de tu archivo JSON
     firebase_admin.initialize_app(cred)
-
     db = firestore.client()
     return db
