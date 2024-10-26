706eba74a2cbceed3c2f063fa497378deb9c2c0f
Alma Peña Valdes <147678686+AlmaPeV@users.noreply.github.com>
2024-10-25 19:36:54 -0600

    Update firebaseConfig.py

--- firebaseConfig.py
+++ firebaseConfig.py
@@ -6,7 +6,7 @@ def init_firebase():
     cred = credentials.Certificate(import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+cred = credentials.Certificate("C:\Users\Z20100\Downloads\maxdiff-a1891-firebase-adminsdk-enu7i-6a840fda73.json")
 firebase_admin.initialize_app(cred))  # Reemplaza esta ruta con la ruta de tu archivo JSON
     firebase_admin.initialize_app(cred)
 
