2ac4959804eb6bf8ef4686cb720339a5ba87cc4f
Alma Peña Valdes <147678686+AlmaPeV@users.noreply.github.com>
2024-10-28 17:31:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-# Autenticación con las credenciales de servicio
-# Asegúrate de que la ruta esté correcta hacia el archivo JSON de credenciales en tu repositorio
-creds = Credentials.from_service_account_file('.devcontainer/encuesta-ko-b5e41359ea53.json')
+# Configura el alcance
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+
+# Carga las credenciales
+creds = Credentials.from_service_account_file('.devcontainer/encuesta-ko-b5e41359ea53.json', scopes=scope)
 client = gspread.authorize(creds)
 
 # Abrir la hoja de cálculo (Google Sheets)
@@ -13,3 +15,4 @@ spreadsheet = client.open_by_key("14MFsN9xYS1EbLWEXq3g6pp4UQ6Fyy2cdsbDdOmcjCDU")
 # Ejemplo de cómo añadir una fila de datos
 sheet.append_row(["Nombre del participante", "Muestra seleccionada", "Ronda"])
 import gspread
+
