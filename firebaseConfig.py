64522ca4986ece17a8bcc7500a8570b5c9ed6e80
Alma Peña Valdes <147678686+AlmaPeV@users.noreply.github.com>
2024-10-28 16:03:17 -0600

    Delete firebaseConfig.py

--- firebaseConfig.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-# Configurar Firebase
-def init_firebase():
-    cred = credentials.Certificate(maxdiff-a1891-firebase-adminsdk-enu7i-6a840fda73.json)  # Reemplaza esta ruta con la ruta de tu archivo JSON
-    firebase_admin.initialize_app(cred)
-    db = firestore.client()
-    return db
