4d1a5cb841f7ddf7dfe0f194c949080a8c8f62cf
Alma Peña Valdes <147678686+AlmaPeV@users.noreply.github.com>
2024-10-28 16:39:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/au
 
 # Autenticación con las credenciales de servicio
 # Asegúrate de que la ruta esté correcta hacia el archivo JSON de credenciales en tu repositorio
-creds = ServiceAccountCredentials.from_json_keyfile_name('config/credentials.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('.devcontainer/encuesta-ko-2b99bdde8769.json', scope)
 client = gspread.authorize(creds)
 
 # Abrir la hoja de cálculo (Google Sheets)
-spreadsheet = client.open("Nombre de la Hoja de Google")
+spreadsheet = client.open("SURVEY-KO")
 sheet = spreadsheet.sheet1  # Si tienes más de una hoja, puedes seleccionar la primera con sheet1
 
 # Ejemplo de cómo añadir una fila de datos
